430f19af2074be46b1be37c89cce17858014f766
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-10-14 14:53:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -193,7 +193,7 @@ resource "aws_instance" "public_instance" {
       "sudo apt update",
       "sudo apt install openjdk-8-jdk",  # Install Java for Ubuntu
       "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \ /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
-      "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \ https://pkg.jenkins.io/debian-stable binary/ | sudo tee \ /etc/apt/sources.list.d/jenkins.list > /dev/null",
+      "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \\ https://pkg.jenkins.io/debian-stable binary/ | sudo tee \\ /etc/apt/sources.list.d/jenkins.list > /dev/null",
       "sudo apt-get update",
       "sudo apt-get install -y jenkins",  # Install Jenkins
       "sudo systemctl start jenkins",  # Start Jenkins
@@ -202,7 +202,7 @@ resource "aws_instance" "public_instance" {
     connection {
       type        = "ssh"
       user        = "ubuntu"  # The user for the Ubuntu AMI
-      private_key = file("home/ubuntu/key1.pem")
+      private_key = file("/home/ubuntu/PK1.ppk")
     }
   }
 }
