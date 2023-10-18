823abcace40bc73a052d7cee00c616d6d6d15c24
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-10-18 22:39:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "aws_route_table_association" "public_subnet_association" {
 # Create a NAT gateway for each public subnet
 resource "aws_nat_gateway" "my_nat_gateway" {
   allocation_id = aws_eip.my_eip.id
-  subnet_id     = element(aws_subnet.public_subnets.*.id, 0)
+  subnet_id     = element(aws_subnet.public_subnets.[1].id, 0)
 
 tags = {
   Name = "nat"
