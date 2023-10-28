46213e811bb0e129c2107fa10063d60a1d23f05a
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-10-28 12:27:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -207,7 +207,7 @@ resource "null_resource" "name" {
     connection {
       type     = "ssh"
       user     = "ubuntu"  # The default username for Ubuntu instances
-      host     = aws_instance.public_instance[count.index].public_ip
+      host     = element(aws_instance.public_instance.*.public_ip, count.index)
       private_key = file("/home/ubuntu/PEM/PK1.pem")  # Replace with your private key file
     }
 
