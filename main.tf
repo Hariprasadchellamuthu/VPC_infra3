621f6476aa94575aeb33c5959f638e36a2f4cbfc
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-11-02 13:27:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -176,6 +176,14 @@ resource "aws_security_group" "public_sg" {
   to_port         = 3306
   protocol        = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+
+# Egress (outbound) rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic to all destinations (0.0.0.0/0)
+  }
 }
 }
 data "aws_ami" "ubuntu22" {
