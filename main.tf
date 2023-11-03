68a0b1c45c1bc955e17bb1b3f0f5511c58b7e759
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-11-03 19:39:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -239,9 +239,8 @@ resource "null_resource" "name" {
     provisioner "remote-exec" {
       inline = [
           "sudo chmod +x /tmp/install_jen.sh",
-          "export PATH=$PATH:/usr/local/bin:/usr/bin",
           "WEB_ROOT=/var/www/html",
-          "RDS_ENDPOINT=$(/usr/local/bin/terraform output -json aws_db_instance_private_rds | /usr/bin/jq -r '..*.endpoint')",
+          "RDS_ENDPOINT=$(terraform output -raw rds_endpoint))",
           "sudo sh /tmp/install_jen.sh"
       ]
     }
@@ -284,9 +283,8 @@ resource "aws_db_instance" "private_rds" {
 
 }
 
-output "aws_db_instance_private_rds" {
-  value = aws_db_instance.private_rds
-  sensitive = true
+output "rds_endpoint" {
+  value = aws_db_instance.private_rds.endpoint
 }
 
 
